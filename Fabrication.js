87990be870cc0fd467469427d93b1d2ae26e13e2
Vamsi <vamsikrishna3296@gmail.com>
2019-08-23 07:28:41 +0530

    Completed Fabrication page without slideshow

---
+++ Fabrication.js
@@ -0,0 +1,11 @@
+$(document).ready(main());
+
+function main(){
+  $('.fabricationSS').slick({
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    arrow: true,
+  });
+}
